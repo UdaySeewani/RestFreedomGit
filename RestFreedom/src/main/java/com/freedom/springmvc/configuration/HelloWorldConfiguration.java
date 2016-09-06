e01d307b1774e653519c85cabd62325c46502890
UdaySeewani <uday.d@tavant.com>
2016-09-06 14:52:46 +0530

    Second commit

--- RestFreedom/src/main/java/com/freedom/springmvc/configuration/HelloWorldConfiguration.java
+++ RestFreedom/src/main/java/com/freedom/springmvc/configuration/HelloWorldConfiguration.java
@@ -3,11 +3,22 @@ package com.freedom.springmvc.configuration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
  
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.freedom.springmvc")
-public class HelloWorldConfiguration {
+public class HelloWorldConfiguration extends WebMvcConfigurerAdapter{
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("swagger-ui.html")
+	      .addResourceLocations("classpath:/META-INF/resources/");
+	 
+	    registry.addResourceHandler("/webjars/**")
+	      .addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
      
  
 }
